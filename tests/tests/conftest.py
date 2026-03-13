6fc0971f10393760a2bc3be8ef72ce73c62037b0
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-03-14 00:18:44 +0530

    Update conftest.py

--- tests/tests/conftest.py
+++ tests/tests/conftest.py
@@ -8,4 +8,4 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 SRC_PATH = PROJECT_ROOT / "src"
 
 if str(SRC_PATH) not in sys.path:
-    sys.path.insert(12, str(SRC_PATH))
+    sys.path.insert(0, str(SRC_PATH))
